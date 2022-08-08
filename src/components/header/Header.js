bdad8ad2a9329a371ebca955e80d22ef3d738002
huuthe1999 <55992625+huuthe1999@users.noreply.github.com>
2022-08-08 11:21:44 +0700

    - Fixed filter country

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,26 +1,32 @@
-import Loading from '../loading.js';
-import './Header.css';
+import Loading from "../loading.js";
+import "./Header.css";
 
-const Header = ({ totalCountry, isLoading }) => {
-	return (
-		<header className='header'>
-			<h2>World Countries Data</h2>
-			<div className='header__subtitle'>
-				Currently, we have{' '}
-				{isLoading ? (
-					<Loading
-						type='spinningBubbles'
-						width={32}
-						height={32}
-						isCustom
-					/>
-				) : (
-					totalCountry
-				)}{' '}
-				countries
-			</div>
-		</header>
-	);
+const Header = ({
+  totalCountry,
+  countFilterCountry,
+  debouncedSearchTerm,
+  isLoading
+}) => {
+  console.log(countFilterCountry);
+  return (
+    <header className="header">
+      <h2>World Countries Data</h2>
+      <div className="header__subtitle">
+        Currently, we have{" "}
+        {isLoading ? (
+          <Loading type="spinningBubbles" width={32} height={32} isCustom />
+        ) : (
+          totalCountry
+        )}{" "}
+        countries
+        {countFilterCountry > 0 && debouncedSearchTerm && (
+          <p className="header__feedback">
+            {countFilterCountry} satisfied the search criteria
+          </p>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
